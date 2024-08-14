e35f30a4d4bff0800e0d9383e61fdbb9b5a18ccc
David Moura <david.dbmoura@gmail.com>
2024-08-14 21:33:46 +0200

    remove comments

--- src/screens/hiddenMenu.js
+++ src/screens/hiddenMenu.js
@@ -9,42 +9,42 @@ function HiddenMenu({ content, navHeight }) {
 
   return (
     <>
-      {/* Button to toggle the hidden menu */}
       <button
         onClick={toggleMenu}
         style={{
           position: 'fixed',
-          top: navHeight, // Keep the button just below the nav bar as you had it
-          right: isOpen ? '300px' : '0', // Position the button to the left of the menu
+          top: navHeight,
+          right: isOpen ? '300px' : '0',
           backgroundColor: '#333',
           color: 'white',
           border: 'none',
           cursor: 'pointer',
           padding: '10px',
           zIndex: 1002, // Ensure the button is above the hidden menu
-          transition: 'right 0.5s ease', // Smooth transition for button movement
+          transition: 'right 0.5s ease',
         }}
       >
         {isOpen ? 'Close' : 'Open'}
       </button>
 
-      {/* Hidden menu positioned below the navigation bar */}
       <div
         style={{
           position: 'fixed',
           top: navHeight,
-          right: isOpen ? '0' : '-300px', // Toggle between showing and hiding the menu
-          height: `calc(100vh - ${navHeight})`, // Adjust the height based on the nav bar
+          right: isOpen ? '0' : '-300px',
+          height: `calc(100vh - ${navHeight})`,
           width: '300px',
           overflowY: 'auto',
-          transition: 'right 0.5s ease', // Smooth transition for menu appearance
-          backgroundColor: 'white', // Keep the background color as white
-          borderLeft: '2px solid #333', // Add a border to the left side of the hidden menu
-          zIndex: 1001, // Make sure it's below the button but above other content
-          color: 'black', // Keep the text color as black
+          transition: 'right 0.5s ease',
+          backgroundColor: 'white',
+          borderLeft: '2px solid #333',
+          zIndex: 1001,
+          color: 'black',
         }}
       >
-        <div style={{ padding: '8px 16px' }}>{content}</div>
+        <div style={{ padding: '8px 16px' }}>
+          {content}
+        </div>
       </div>
     </>
   );
