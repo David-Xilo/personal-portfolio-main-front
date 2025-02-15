427e066bbd39af9f43c98e9f9f0d284ff538455f
David Moura <david.dbmoura@gmail.com>
2025-02-15 15:11:45 +0100

    fix webpack

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -42,7 +42,7 @@ module.exports = {
       // CSS files
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       // Images and fonts
       {
