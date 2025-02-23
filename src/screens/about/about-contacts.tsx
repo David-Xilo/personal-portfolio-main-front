2379aee42471d208c9694941c0dc3db5e6aed053
David Moura <david.dbmoura@gmail.com>
2025-02-23 18:00:04 +0100

    remove hidden menu from about

--- src/screens/about/about-contacts.tsx
+++ src/screens/about/about-contacts.tsx
@@ -1,23 +1,15 @@
 import * as React from 'react'
 
 import {useEffect} from 'react'
-import {SET_HIDDEN_NAV} from '../../reducers/hidden-menu-reducer'
+import {CLEAR_HIDDEN_NAV, SET_HIDDEN_NAV} from '../../reducers/hidden-menu-reducer'
 import {ContactScreen} from './contact-screen'
 import {useIntroGetApi} from '../../hooks/intro-rest'
 import {SubMenuProps} from '../../reducers/sub-menu-reducer'
 
-const AboutContactsHiddenMenu: React.FC = () => {
-  const data = useIntroGetApi('/about/intro')
-
-  return <p>{data.message}</p>
-}
 
 const AboutContactsScreen: React.FC<SubMenuProps> = ({hiddenMenuDispatch}) => {
   useEffect(() => {
-    hiddenMenuDispatch({
-      type: SET_HIDDEN_NAV,
-      component: AboutContactsHiddenMenu,
-    })
+    hiddenMenuDispatch({type: CLEAR_HIDDEN_NAV})
   }, [hiddenMenuDispatch])
 
   return (
