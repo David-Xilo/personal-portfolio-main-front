5a28d32856893b7c92130ef5397f7be16f8c4342
David Moura <david.dbmoura@gmail.com>
2025-06-24 21:44:54 +0200

    cleanup handlers

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,16 +3,6 @@ import {http, HttpResponse} from 'msw'
 const domain = process.env.REACT_APP_API_URL
 
 export const handlers = [
-  http.get(`${domain}/about/intro`, () => {
-    const intro = {
-      message: "Hey! I’m David—great to see you here!\n " +
-        "I built a simple portfolio page a while back but kept postponing it—well, it’s finally live!\n " +
-        "I’m a Senior Software Engineer specializing in backend. " +
-        "I have a LOT of interests and tend to explore them fully, so expect to see some interesting projects here!\n " +
-        "You’ll also find my contact info and a peek into my passions. Enjoy exploring!",
-    }
-    return HttpResponse.json(intro)
-  }),
   http.get(`${domain}/about/contact`, () => {
     const contact = {
       message: {
@@ -56,16 +46,6 @@ export const handlers = [
       ]
     }
     return HttpResponse.json(contact)
-  }),
-  http.get(`${domain}/tech/intro`, () => {
-    const intro = {
-      message: "Technology is always improving — and so am I.\n " +
-        "Technology is my bread and butter. I've grown around it, worked with it, and played with it — " +
-        "It's part of my personality and I try to learn and explore it as much as I can.\n " +
-        "I'll post my personal projects here as a way to keep myself accountable.\n" +
-        " Have fun exploring them!",
-    }
-    return HttpResponse.json(intro)
   }),
   http.get(`${domain}/tech/projects`, () => {
     const projects = {
@@ -124,19 +104,6 @@ export const handlers = [
       ],
     }
     return HttpResponse.json(projects)
-  }),
-  http.get(`${domain}/games/intro`, () => {
-    const intro = {
-      message: "This section is all about games.\n " +
-        "Since I was a kid I've loved video games, in fact, they were one of the main reasons I got interested in computers.\n " +
-        "This page might be empty (it is for sure emptier than I'd like), but I'll keep adding new games I've made here. " +
-        "Who knows — maybe one day you'll recognize one of the names!",
-    }
-    return HttpResponse.json(intro)
-  }),
-  http.get(`${domain}/games/genres`, () => {
-    const genres = ['undefined', 'strategy', 'table top']
-    return HttpResponse.json(genres)
   }),
   http.get(`${domain}/games/projects`, () => {
     const projects = {
@@ -237,15 +204,6 @@ export const handlers = [
       ],
     }
     return HttpResponse.json(games_played_carousel)
-  }),
-  http.get(`${domain}/finance/intro`, () => {
-    const intro = {
-      message: "Finance is one of my passions. I read about it, study it, and pay attention to it in my daily life.\n " +
-        "Since I was young, I’ve followed the stock market and economic news. " +
-        "During my master’s thesis, I finally combined my passions for technology and finance—and I loved it.\n " +
-        "Now, I’ll post my finance-related personal projects here, along with any certifications or extra courses I complete in my free time.",
-    }
-    return HttpResponse.json(intro)
   }),
   http.get(`${domain}/finance/projects`, () => {
     const projects = {
